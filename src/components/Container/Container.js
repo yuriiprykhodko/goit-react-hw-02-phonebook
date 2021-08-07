73c54edd1b28cf4dc366bc13b801180cc0a799fc
yuriyprihodko158 <74249419+yuriyprihodko158@users.noreply.github.com>
2021-08-07 19:01:06 +0300

    001

---
+++ src/components/Container/Container.js
@@ -0,0 +1,14 @@
+import PropTypes from 'prop-types';
+import './Container.css';
+
+const Container = ({ children }) => <div className="сontainer">{children}</div>;
+
+Container.propTypes = {
+  children: PropTypes.array.isRequired,
+};
+
+Container.propTypes = {
+  children: PropTypes.array.isRequired,
+};
+
+export default Container;
